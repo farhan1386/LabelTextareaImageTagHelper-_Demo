9350f3c8ed6688d149ed077533f76af805a213a2
Farhan Ahmed <farhan1086@gmail.com>
2020-01-04 14:58:18 +0530

    initial commit

---
+++ LabelTextareaImageTagHelper _Demo/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabelTextareaImageTagHelper__Demo.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(255)]
+        public string Description { get; set; }
+    }
+}
